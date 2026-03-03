22dd2e43efec4879c6315ba6c8db619c22ea867a
SD-gif <alem6516@naver.com>
2026-03-03 19:00:38 +0900

    initial commit

---
+++ src/sdj/practice0303/DiceDemo.java
@@ -0,0 +1,25 @@
+package sdj.practice0303;
+
+import java.util.Scanner;
+
+public class DiceDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("주사위를 몇 번 굴릴까요? : ");
+        int num = sc.nextInt();
+
+        Dice dice = new Dice();
+
+        while(num > 0){
+            dice.roll();
+            num--;
+        }
+    }
+}
+
+class Dice {
+    public void roll(){
+        System.out.println((int) (Math.random()*6) + 1);
+    }
+}
